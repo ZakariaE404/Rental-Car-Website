491cf92197742989337a9fad9fa2e32ab1fa4f29
zakariaE404 <zakariae.benhachem05@gmail.com>
2026-02-17 11:18:05 -0900

    fixing some deployment stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  // Crucial for GitHub Pages
-  base: "/Rental-Car-Website/",
-
   plugins: [react()],
-
+  base: '/Rental-Car-Website/', // MUST match your repo name
   resolve: {
     alias: {
-      // Since your files are in the root, @ should point to root
       '@': path.resolve(__dirname, './'),
     },
   },
-  build: {
-    // Ensures Vite knows index.html is the entry point in the root
-    outDir: 'dist',
-  }
-});
+})
