ae97d71e054a20de5e537512fdfb1bfa2595e439
zakariaE404 <zakariae.benhachem05@gmail.com>
2026-02-17 12:18:49 -0900

    fixxexs

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/Rental-Car-Website/',
-  root: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-});
+})
